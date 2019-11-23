370cdf8d35d16ba64ef5edc6fa853c4292fbf2b0
Divyaansh <divyaansh03anu@gmail.com>
2019-11-24 00:58:22 +0530

    Task on If statement

---
+++ Ifstatement.java
@@ -0,0 +1,22 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+   
+   	int[] Array = {5,5};
+   	
+   	if(Array[1] > Array[0]) {
+   			
+   		System.out.println("2nd number is greater than 1st");
+   		
+   		} else if(Array[0] > Array[1]) {
+   		
+   		System.out.println("1st number is greater than 2nd");
+   		
+   		} else {
+   		
+   		System.out.println("The number is same");
+   		 
+   		 }
+ 
+  }
+}
+
