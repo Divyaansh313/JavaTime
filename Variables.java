6da128719017a1d760ef06efd5209fd2410ba18d
Divyaansh <divyaansh03anu@gmail.com>
2019-11-23 16:47:22 +0530

    Finding the number of characters in your name

---
+++ Variables.java
@@ -0,0 +1,34 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+  
+	String FirstName = "Divyaansh";
+	String LastName = "Jain";
+	
+	int a,b;
+	
+	a = FirstName.length();
+	b = LastName.length();
+	
+	int num;
+	
+	num = a+b;
+	
+	System.out.println(" The number of characters in my name is " + num + ".");
+  
+  
+  
+  
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+//Use of all variables and understanding them
