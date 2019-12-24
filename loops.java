3dfbcfb511e539847141a9bcf0963f457491cba4
Divyaansh <divyaansh03anu@gmail.com>
2019-12-24 20:59:55 +0530

    printing first ten triangular numbers

--- loops.java
+++ loops.java
@@ -19,7 +19,11 @@ public class Main
 		
 		for (int y=5; y > 0; y--){
 		    
-		    System.out.println(2 * y);
+		    System.out.println(2 * y); //Loop for printing even numbers from 1 to 10 in reverse fashion
+		}
+		for (int y=1; y<=10; y++){
+		    
+		    System.out.println((y * (y+1)) / 2); //This loop is for printing first 10 triangular numbers which simply means the number of objects that can be arranged in an equilateral triangle
 		}
 	}
 }
